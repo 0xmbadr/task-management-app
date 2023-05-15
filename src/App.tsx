877a640966d49d45b9ab4d6212b895bfc9fa668a
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-15 15:04:26 +0200

    feat: add light/dark theme feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+
 function App() {
+  const [colorTheme, setColorTheme] = useState('light');
+  const handleClick = () => {
+    if (colorTheme == 'light') {
+      setColorTheme('dark');
+    } else {
+      setColorTheme('light');
+    }
+  };
+
   return (
-    <div>
-      <p>Hello from React</p>
+    <div className={`App ${colorTheme}`}>
+      <button onClick={() => handleClick()}>Toggle Theme</button>
     </div>
   );
 }
