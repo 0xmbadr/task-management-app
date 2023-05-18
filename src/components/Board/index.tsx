985f277b0d356826adfdaf28dd9403c8f2233c9f
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-18 21:16:33 +0200

    [SCSS] => (Board) add UI for empty board.

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,9 +1,20 @@
+import Button from '../standard/Button';
+
 type BoardProps = {
   hideSideNav: boolean;
 };
 
 const Board = ({ hideSideNav }: BoardProps) => {
   const onHide = hideSideNav ? 'Board__full' : '';
-  return <div className={`Board ${onHide}`}>Board Section lies here!</div>;
+  return (
+    <div className={`Board ${onHide} Board__empty`}>
+      <p className="Board__empty-txt">
+        This board is empty. Create a new column to get started.
+      </p>
+      <div className="Board__empty-btn">
+        <Button>&nbsp; + Create New Board &nbsp;</Button>
+      </div>
+    </div>
+  );
 };
 export default Board;
