c720e5f50910f8961c3c3904f4f357fdab872235
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-19 22:13:54 +0200

    create new board from side nav

--- src/components/SideNav/Tab.tsx
+++ src/components/SideNav/Tab.tsx
@@ -1,13 +1,20 @@
 import { TbLayoutBoardSplit } from 'react-icons/tb';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../app/slices/modalSlice';
 type TabProps = {
   name?: string;
   addNew?: boolean;
 };
 
 const Tab = ({ name, addNew }: TabProps) => {
+  // store
+  const dispatch = useDispatch();
+
   if (addNew) {
     return (
-      <button className="SideNav__tab SideNav__addNew">
+      <button
+        className="SideNav__tab SideNav__addNew"
+        onClick={() => dispatch(openModal({ ModalType: 'AddBoard' }))}>
         <TbLayoutBoardSplit /> + Create A New Board
       </button>
     );
