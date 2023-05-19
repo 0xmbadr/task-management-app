22718c62284c69c5842b4e0dd272df43928d5db1
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-19 04:11:23 +0200

    [feat] add event listener to handle closing the modal using the escap key.

--- src/components/standard/Modal.tsx
+++ src/components/standard/Modal.tsx
@@ -1,12 +1,27 @@
 import { ImCross } from 'react-icons/im';
 import { useDispatch } from 'react-redux';
 import { closeModal } from '../../app/slices/modalSlice';
+import { useEffect } from 'react';
 type ModalProps = {
   children: JSX.Element;
 };
 
 const Modal = ({ children }: ModalProps) => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleCloseModal = (e: KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        dispatch(closeModal());
+
+        // remove focus from clicked button (that triggered Modal)
+        (document.activeElement as HTMLElement).blur();
+      }
+    };
+    document.addEventListener('keydown', handleCloseModal);
+    // remove event listener, no point of tracking the clicks after closing the modal
+    return () => document.removeEventListener('keydown', handleCloseModal);
+  }, [dispatch]);
 
   return (
     <div className="Overlay" onClick={() => dispatch(closeModal())}>
