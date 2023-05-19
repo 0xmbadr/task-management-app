cc8d904d306b74b27d3f59ed0b23c189ff3dd860
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-19 20:54:40 +0200

    [scss] styling errors produced from react-hook-form

--- src/components/Modals/AddBoard.tsx
+++ src/components/Modals/AddBoard.tsx
@@ -1,13 +1,15 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import Button from '../standard/Button';
 import Modal from '../standard/Modal';
-import { IBoard } from '../../@types/data';
+import { IBoard, IColumn } from '../../@types/data';
 import { nanoid } from '@reduxjs/toolkit';
 
 const AddBoard = () => {
   const {
     register,
     handleSubmit,
+    control,
+    watch,
     formState: { errors },
   } = useForm<IBoard>({
     defaultValues: {
@@ -23,15 +25,35 @@ const AddBoard = () => {
     },
   });
 
+  // Handle Dynamic fields
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'columns',
+  });
+
+  const watchFieldArrays = watch('columns');
+
+  const controlledFields = fields.map((field, index) => {
+    return {
+      ...field,
+      ...watchFieldArrays?.[index],
+    };
+  });
+
   const onSubmit: SubmitHandler<IBoard> = (data) => {
     console.log(data);
   };
 
   const registerOptions = {
     name: {
-      required: 'name field is required!',
+      required: true,
       validate: (value: string | undefined) =>
         !value?.includes('o') || 'name must not include o',
+    },
+    columns: {
+      required: true,
+      validate: (value: string | undefined) =>
+        !value?.includes('o') || 'column name must not include o',
     },
   };
 
@@ -41,6 +63,8 @@ const AddBoard = () => {
         <div className="AddNew__heading">
           <h2>Add New Board</h2>
         </div>
+
+        {/* ======= Form name field ========= */}
         <div className="AddNew__wrapper">
           <p className="AddNew__subtitle">name</p>
           <label
@@ -51,16 +75,46 @@ const AddBoard = () => {
                 {errors.name.message}
               </span>
             )}
+            {errors.name?.type == 'required' && (
+              <span className="AddNew__label--errTxt">
+                name field is required!
+              </span>
+            )}
           </label>
         </div>
+
+        {/* ======= Form columns field ========= */}
         <div className="AddNew__wrapper">
           <p className="AddNew__subtitle">Column</p>
-          <label className="AddNew__label">
-            <input
-              type="text"
-              {...register('columns.0.name', { required: true })}
-            />
-          </label>
+
+          <ul>
+            {controlledFields.map((item: IColumn, index: number) => (
+              <li className="" key={item.id}>
+                <label
+                  className={`AddNew__label ${
+                    errors.columns?.[index]?.name && 'AddNew__label--err'
+                  }`}>
+                  <input
+                    {...register(
+                      `columns.${index}.name`,
+                      registerOptions.columns,
+                    )}
+                  />
+                  {errors.columns?.[index]?.name?.type == 'validate' && (
+                    <span className="AddNew__label--errTxt">
+                      {errors.columns[index]?.name?.message}
+                    </span>
+                  )}
+
+                  {errors.columns?.[index]?.name?.type == 'required' && (
+                    <span className="AddNew__label--errTxt">
+                      column field is required!
+                    </span>
+                  )}
+                </label>
+              </li>
+            ))}
+          </ul>
 
           <Button small colorTheme style={{ marginTop: '0.5rem' }}>
             + Add New Column
